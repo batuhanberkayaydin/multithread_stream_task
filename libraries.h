05a10f5ec5a7e8b06ae006736604835a0ff4e479
batuhanberkayaydin <batuhanberkayaydin@gmail.com>
2022-05-31 01:50:08 +0300

    Some Changes

--- libraries.h
+++ libraries.h
@@ -1,3 +1,12 @@
+/**
+ * @file libraries.h
+ * @author Batuhan
+ * @brief 
+ */
+
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
 #include "iostream"
 #include <opencv2/opencv.hpp>
 #include <random>
@@ -5,3 +14,6 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
+
+
+#endif // LIBRARIES_H
